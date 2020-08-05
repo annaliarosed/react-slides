d75c37c550fc1bbc7ac9db556b24bb36d1c5450b
annaliarosed <56635645+annaliarosed@users.noreply.github.com>
2020-08-05 10:42:51 +0300

    a lot of changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const slides = [
+    {
+        text: "1", 
+        color: "#fe8e79",
+        key: '1'
+    },
+    {
+        text: "2",
+        color: "#fd7860", 
+        key: '2'  
+    },
+    {
+        text: "3",
+        color: "#fd6347",
+        key: '3'
+    }
+  ]
+
+const App = () => {
+    const LEFT_ARROW = 37;
+    const RIGHT_ARROW = 39;
+    const [activeSlideIndex, setActiveSlideIndex] = useState(0)
+    
+    useEffect(() => {
+            window.addEventListener('keydown', listener)
+        return () => {
+            window.removeEventListener('keydown', listener)
+        }
+    }, [activeSlideIndex])
+    
+    const nextSlide = () => {
+        if (activeSlideIndex + 1 < slides.length) {
+            setActiveSlideIndex(prevState => prevState + 1)
+        }   else {
+            setActiveSlideIndex(slides.length - slides.length)
+        }
+    }
+    
+    const prevSlide = () => {
+        if (activeSlideIndex > 0) {
+            setActiveSlideIndex(prevState => prevState - 1)
+        } else {
+            setActiveSlideIndex(slides.length - 1)
+        }
+    }
+    
+    const listener = (e) => {
+        if (e.keyCode === RIGHT_ARROW) {
+            nextSlide()
+        } else if (e.keyCode === LEFT_ARROW) {
+            prevSlide()
+        }
+    }
+    
+   return (
+    <div 
+      className="wrapper" 
+      style={{backgroundColor: slides[activeSlideIndex].color}}
+    >
+      {slides.map((slide, index) => {
+          return <div
+                      key={slide.key}
+                      className={activeSlideIndex === index ? "active-slide" : "idle-slide"}  
+                  >
+                      {slide.text}
+                  </div>
+      })}  
+      <div className="arrow-wrapper">
+          <span onClick={prevSlide}>🡨</span>
+          <span onClick={nextSlide}>🡪</span>
+      </div>
+      <footer className="buttons">
+          {slides.map((slide, index) => {
+            return <button 
+                  className={activeSlideIndex === index ? "active-button" : 'idle-button'}
+                  key={slide.key}
+                  onClick={() => setActiveSlideIndex(index)}
+              />
+          })}
+      </footer>
+    </div>    
+  )
 }
 
-export default App;
+export default App
